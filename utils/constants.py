7975af73448227b7e20fc6378ad84fa281a59a0f
e-rwan <e-rwan@e-rwan.com>
2026-06-20 09:17:45 +0200

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -9,7 +9,7 @@ from pathlib import Path
 def get_base_path() -> Path:
     if getattr(sys, 'frozen', False):
         # Exécutable compilé (PyInstaller)
-        return Path(sys.executable).parent
+        return Path(sys.executable).parent.parent
     else:
         # Script lancé en mode normal
         return Path(__file__).resolve().parent
